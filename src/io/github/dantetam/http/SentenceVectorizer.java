9e6c0a4de3be10eee5ea72ffd6be0dbef2bf8175
dantetam <dante.tam1@gmail.com>
2017-05-17 15:24:30 -0700

    Impl. dependency structure to vector

--- src/io/github/dantetam/http/SentenceVectorizer.java
+++ src/io/github/dantetam/http/SentenceVectorizer.java
@@ -1,19 +1,41 @@
 package io.github.dantetam.http;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import edu.stanford.nlp.trees.TypedDependency;
+import io.github.dantetam.parser.ParseGrammarResult;
+import io.github.dantetam.parser.StellaDependencyParser;
+
+import java.util.Set;
+
 public class SentenceVectorizer {
 
 	public static final void main(String[] args) {
 		List<String> listSentences = new ArrayList<>();
 		listSentences.add("Hello, this is a test this is a test of the outdoor warning system system test test test");
+		listSentences.add("a is");
 		Map<String, Integer> results = uniqueWords(listSentences);
 		findWordIdMap(results, 5);
+		
+		encodeSentenceAsGrammarVec(null, "You walk alone every night in the dark", 0);
+	}
+	
+	public static int lengthLongestSentence(List<String> sentences) {
+		int maxLen = -1;
+		for (String sentence: sentences) {
+			String[] words = sentence.split(" ");
+			if (maxLen == -1 || words.length > maxLen) {
+				maxLen = words.length;
+			}
+		}
+		return maxLen;
 	}
 	
 	public static Map<String, Integer> uniqueWords(List<String> sentences) {
@@ -36,23 +58,42 @@ public class SentenceVectorizer {
 		}
 		Map<String, Integer> sorted = MapUtil.sortByValueDescending(uniqueWords);
 		Map<String, Integer> results = new LinkedHashMap<>();
-		System.out.println(sorted.entrySet());
 		int i = 0;
 		for (Entry<String, Integer> entry: sorted.entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
-			results.put(entry.getKey(), entry.getValue());
-			i++;
-			if (i == desiredNum) {
+			if (i >= desiredNum) {
 				break;
 			}
+			results.put(entry.getKey(), i);
+			i++;
 		}
 		return results;
 	}
 	
-	public int[] encodeSentenceAsWordVec(String sentence) {
+	public static int[] encodeSentenceAsWordVec(Map<String, Integer> importantWords, String sentence) {
+		int vecLength = importantWords.size();
+		
 		String[] words = sentence.split(" ");
-		int[] vector = new int[words.length];
+		int[] vector = new int[vecLength];
+		for (int i = 0; i < vecLength; i++) {
+			vector[i] = 0;
+		}
+		
 		//for (String str)
+		for (String word: words) {
+			if (importantWords.containsKey(word)) {
+				vector[importantWords.get(word)] = 1;
+			}
+		}
+		return vector;
+	}
+	
+	public static int[] encodeSentenceAsGrammarVec(Map<String, Integer> grammarRelationIds, String sentence, int sizeMaxSentence) {
+		int[] vector = new int[sizeMaxSentence*sizeMaxSentence];
+		ParseGrammarResult grammarResult = StellaDependencyParser.parseGrammarStructure(sentence).get(0);
+		Collection<TypedDependency> dependencies = grammarResult.grammarStructure.allTypedDependencies();
+		for (TypedDependency dependency: dependencies) {
+			System.out.println(dependency.gov() + " " + dependency.dep() + " " + dependency.reln());
+		}
 		return vector;
 	}
 	
